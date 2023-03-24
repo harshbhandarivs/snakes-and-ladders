4db77ad1c279b3bfab6d62d0257cf0293c53d840
Harsh Bhandari <harshb@sahaj.ai>
2023-03-24 10:11:37 +0530

    Move Application logic from Main to App class

--- app/src/main/java/com/snakes_and_ladders/Main.java
+++ app/src/main/java/com/snakes_and_ladders/Main.java
@@ -1,49 +1,11 @@
 package com.snakes_and_ladders;
 
 
-import com.snakes_and_ladders.entity.board.Board;
-import com.snakes_and_ladders.entity.dice.Dice;
-import com.snakes_and_ladders.entity.dice.DiceImpl;
-import com.snakes_and_ladders.entity.ladder.Ladder;
-import com.snakes_and_ladders.entity.ladder.LadderMap;
-import com.snakes_and_ladders.entity.snake.Snake;
-import com.snakes_and_ladders.entity.snake.SnakeMap;
-import com.snakes_and_ladders.printer.PrinterImpl;
-import com.snakes_and_ladders.printer.Prompt;
-
 public class Main {
 
     public static void main(String[] args) {
-        Dice diceRoll = new DiceImpl();
-        Prompt prompt = new Prompt(new PrinterImpl());
-        Snake[] snakes = {
-                new Snake(18, 2),
-                new Snake(25, 8),
-                new Snake(38, 11),
-                new Snake(41, 19),
-                new Snake(59, 21),
-                new Snake(72, 12),
-                new Snake(78, 7),
-                new Snake(86, 31),
-                new Snake(92, 26),
-                new Snake(97, 5),
-        };
-        Ladder[] ladders = {
-                new Ladder(9, 32),
-                new Ladder(12, 53),
-                new Ladder(17, 90),
-                new Ladder(21, 50),
-                new Ladder(27, 66),
-                new Ladder(29, 42),
-                new Ladder(44, 73),
-                new Ladder(63, 88)
-        };
-        LadderMap ladderPositions = new LadderMap(ladders);
-        SnakeMap snakePositions = new SnakeMap(snakes);
-        Board board = new Board(snakePositions, ladderPositions);
-        Game game = new Game(diceRoll, prompt, board);
-        game.play();
-        System.exit(1);
+        App app = new App();
+        app.start();
     }
 
 }
